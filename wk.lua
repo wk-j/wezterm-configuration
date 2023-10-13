b32b54acf4306949ade67ce68968ef72b0ce8af0
wk <somnuk.wk@gmail.com>
2023-10-14 01:16:24 +0700

    Update window_frame font size to 15

--- wk.lua
+++ wk.lua
@@ -1,9 +1,12 @@
 local wezterm = require 'wezterm'
 local config = { }
+local act = wezterm.action
 
 config.window_frame = {
-    font = wezterm.font "Syne Mono",
-    font_size = 14,
+    -- font = wezterm.font "DaddyTimeMono Nerd Font",
+    font = wezterm.font "ShureTechMono Nerd Font",
+    -- font = wezterm.font "OpenDyslexic",
+    font_size = 15,
 }
 
 config.window_padding = {
@@ -28,15 +31,51 @@ config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
 config.cursor_blink_rate = 500
 
-config.background = {
-    {
-        source = {
-            File = "/Users/wk/.config/wezterm/bg/k.jpeg",
-        },
-        repeat_x = 'NoRepeat',
-        repeat_y = 'NoRepeat',
-        opacity = 0.99
-    }
+config.leader = { key = ";", mods = "CMD", timeout_milliseconds = 1000 }
+config.keys = {
+    { key = "-", mods = "LEADER",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+    { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
 }
+
+config.status_update_interval = 1000
+wezterm.on("update-right-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then stat = window:active_key_table() end
+  if window:leader_is_active() then stat = "LDR" end
+
+  -- Current working directory
+  local basename = function(s)
+    -- Nothign a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+  local cwd = basename(pane:get_current_working_dir())
+  -- Current command
+  local cmd = basename(pane:get_foreground_process_name())
+
+  -- Time
+  local time = wezterm.strftime("%H:%M")
+
+  -- Let's add color to one of the components
+  window:set_right_status(wezterm.format({
+    -- Wezterm has a built-in nerd fonts
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = "FFB86C" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+    { Text = "  " },
+  }))
+end)
 
 return config
