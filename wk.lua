32afbae7dd745c7ca6150e7b7a2ddd851440a843
wk <somnuk.wk@gmail.com>
2023-10-13 22:29:25 +0700

    Update font and window padding in wk.lua config

--- wk.lua
+++ wk.lua
@@ -2,17 +2,32 @@ local wezterm = require 'wezterm'
 local config = { }
 
 config.window_frame = {
-    font = wezterm.font "Agave Nerd Font",
-    font_size = 14
+    font = wezterm.font "Syne Mono",
+    font_size = 14,
+    border_bottom_height = '0cell'
+}
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 10,
+    bottom = 0
 }
 
 config.initial_cols = 120
 config.color_scheme = 'Batman'
+-- config.color_scheme = 'carbonfox'
 config.font = wezterm.font "Mononoki Nerd Font"
-config.font_size = 14.5
+config.font_size = 14.3
 config.line_height = 1.5
 config.text_background_opacity = 0.3
-config.window_background_opacity = 0.98
+config.window_background_opacity = 0.95
 config.window_decorations = "RESIZE"
+
+-- config.cursor_tickness = "200%"
+-- config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+config.cursor_blink_rate = 500
 
 return config
